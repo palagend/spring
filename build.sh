4d2a7ecf9899ea00b16c4133b996adf2d9c6b7f5
Grain Stark <palagend@gmail.com>
2018-06-12 10:28:09 +0800

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+REGISTRY="${REGISTRY:-hub.fzyun.io:5000}"
+
+set -e
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+repo="$(basename "$PWD")"
+
+projects=( "$@" )
+if [ ${#projects[@]} -eq 0 ]; then
+    projects=( */ )
+fi
+projects=( "${projects[@]%/}" )
+
+for project in "${projects[@]}"; do
+    cd "$project"
+    image="founder/$project"
+    docker build -t "$image" .
+    docker tag "$image" "$REGISTRY/$image"
+    docker push "$REGISTRY/$image"
+    docker rmi "$REGISTRY/$image"
+    cd -
+done
+
